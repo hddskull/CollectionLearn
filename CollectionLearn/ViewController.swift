b2f4262e8d356218359dc684273fe0ce9b9d1fb4
Max Macbook <maxdgladkov@gmail.com>
2021-09-05 17:58:36 +0300

    Initial Commit

---
+++ CollectionLearn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionLearn
+//
+//  Created by Gladkov Maxim on 05.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
